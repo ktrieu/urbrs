42051d9cb6df7783bec4ac58f3023fd61f9f994d
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-12-19 02:02:00 -0500

    Add winit skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowAttributes, WindowId},
+};
+
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(WindowAttributes::default().with_title("urbrs"))
+                .unwrap(),
+        )
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App { window: None };
+    let _ = event_loop.run_app(&mut app);
 }

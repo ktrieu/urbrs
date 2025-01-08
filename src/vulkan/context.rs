b3ca6c1bafe27fbafa8f5a30c19508c40a623bf1
Kevin Trieu <kevin.trieu5813@gmail.com>
2025-01-08 02:10:06 -0500

    Cleanup context creation code

--- src/vulkan/context.rs
+++ src/vulkan/context.rs
@@ -1,5 +1,7 @@
 use std::ffi::{c_void, CStr};
+use std::fmt::Display;
 
+use ash::prelude::VkResult;
 use ash::{Entry, Instance};
 
 use super::device::Device;
@@ -30,13 +32,94 @@ unsafe extern "system" fn debug_callback(
     return ash::vk::FALSE;
 }
 
+#[derive(Debug)]
+pub enum ContextCreateError {
+    VkError(ash::vk::Result),
+    UnsupportedValidationLayers(Vec<String>),
+    UnsupportedExtensions(Vec<String>),
+}
+
+impl From<ash::vk::Result> for ContextCreateError {
+    fn from(value: ash::vk::Result) -> Self {
+        Self::VkError(value)
+    }
+}
+
+impl Display for ContextCreateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ContextCreateError::VkError(vk_err) => writeln!(f, "vulkan error: {vk_err}"),
+            ContextCreateError::UnsupportedValidationLayers(layers) => {
+                let layers = layers.join(", ");
+                writeln!(f, "unsupported validation layers: {layers}")
+            }
+            ContextCreateError::UnsupportedExtensions(exts) => {
+                let exts = exts.join(", ");
+                writeln!(f, "unsupported instance extensions: {exts}")
+            }
+        }
+    }
+}
+
 impl<'a> Context<'a> {
     const REQUIRED_VALIDATION_LAYERS: &'static [&'static CStr; 1] =
         &[c"VK_LAYER_KHRONOS_validation"];
 
     const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
 
-    pub fn new() -> Result<Self, ash::vk::Result> {
+    const REQUIRED_INSTANCE_EXTENSIONS_BASE: &'static [&'static CStr; 1] =
+        &[ash::vk::EXT_DEBUG_UTILS_NAME];
+
+    fn get_required_instance_extensions() -> Vec<&'static CStr> {
+        Self::REQUIRED_INSTANCE_EXTENSIONS_BASE
+            .iter()
+            .copied()
+            .collect()
+    }
+
+    fn get_unsupported_instance_extensions<'b>(
+        required_extensions: &'b Vec<&CStr>,
+        extension_props: &Vec<ash::vk::ExtensionProperties>,
+    ) -> Vec<&'b CStr> {
+        required_extensions
+            .iter()
+            .copied()
+            .filter(|required| {
+                extension_props
+                    .iter()
+                    .find(|extension| {
+                        let extension_name = extension
+                            .extension_name_as_c_str()
+                            .expect("extension name should be valid CStr");
+                        *required == extension_name
+                    })
+                    .is_none()
+            })
+            .collect()
+    }
+
+    fn get_unsupported_validation_layers<'b>(
+        required_layers: &'b [&CStr],
+        layer_props: &Vec<ash::vk::LayerProperties>,
+    ) -> Vec<&'b CStr> {
+        required_layers
+            .iter()
+            .copied()
+            .filter(|required| {
+                layer_props
+                    .iter()
+                    .find(|layer| {
+                        let layer_name = layer
+                            .layer_name_as_c_str()
+                            .expect("layer name should be valid CStr");
+                        *required == layer_name
+                    })
+                    .is_none()
+            })
+            .collect()
+    }
+
+    pub fn new() -> Result<Self, ContextCreateError> {
         let entry = Entry::linked();
 
         let app_info = ash::vk::ApplicationInfo::default()
@@ -45,35 +128,61 @@ impl<'a> Context<'a> {
             .application_name(c"urbrs");
         let mut create_info = ash::vk::InstanceCreateInfo::default().application_info(&app_info);
 
-        let mut enabled_layers: Vec<*const i8> = Vec::new();
+        let required_extensions = Self::get_required_instance_extensions();
+        let supported_extensions = unsafe { entry.enumerate_instance_extension_properties(None)? };
+
+        let unsupported_extensions =
+            Self::get_unsupported_instance_extensions(&required_extensions, &supported_extensions);
+
+        if unsupported_extensions.len() > 0 {
+            let unsupported_extensions = unsupported_extensions
+                .iter()
+                .map(|e| {
+                    e.to_str()
+                        .expect("extension name should be valid str")
+                        .to_string()
+                })
+                .collect();
+            return Err(ContextCreateError::UnsupportedExtensions(
+                unsupported_extensions,
+            ));
+        }
+
+        let enabled_extension_names: Vec<*const i8> =
+            required_extensions.iter().map(|e| e.as_ptr()).collect();
+        create_info = create_info.enabled_extension_names(&enabled_extension_names);
+
         let available_validation_layers = unsafe { entry.enumerate_instance_layer_properties()? };
 
-        let exts = [ash::vk::EXT_DEBUG_UTILS_NAME.as_ptr()];
+        let mut enabled_layers: Vec<*const i8> = Vec::new();
 
         if Self::VALIDATION_ENABLED {
-            let all_validation_layers_supported =
-                Self::REQUIRED_VALIDATION_LAYERS.iter().all(|l| {
-                    available_validation_layers
-                        .iter()
-                        .find(|avail| match avail.layer_name_as_c_str() {
-                            Ok(name) => *l == name,
-                            Err(_) => false,
-                        })
-                        .is_some()
-                });
+            let unsupported_layers = Self::get_unsupported_validation_layers(
+                Self::REQUIRED_VALIDATION_LAYERS,
+                &available_validation_layers,
+            );
 
-            if !all_validation_layers_supported {
-                // TODO: use a real error type instead of giving up and dying
-                panic!("validation layer not supported!");
+            if unsupported_layers.len() > 0 {
+                let unsupported_layers = unsupported_layers
+                    .iter()
+                    .map(|e| {
+                        e.to_str()
+                            .expect("layer name should be valid str")
+                            .to_string()
+                    })
+                    .collect();
+
+                return Err(ContextCreateError::UnsupportedValidationLayers(
+                    unsupported_layers,
+                ));
             }
 
-            for l in Self::REQUIRED_VALIDATION_LAYERS {
-                enabled_layers.push(l.as_ptr());
-            }
+            enabled_layers = Self::REQUIRED_VALIDATION_LAYERS
+                .iter()
+                .map(|l| l.as_ptr())
+                .collect();
 
             create_info = create_info.enabled_layer_names(enabled_layers.as_slice());
-
-            create_info = create_info.enabled_extension_names(&exts);
         }
 
         // Safety: It's safe to use create_instance any time if it comes from Entry::linked.

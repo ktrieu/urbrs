17c9e9e10c5aa3d5009727c5ae12e8c05322e306
Kevin Trieu <kevin.trieu5813@gmail.com>
2025-03-02 02:34:38 -0500

    Add GPU allocator to context

--- urbrs/src/vulkan/context.rs
+++ urbrs/src/vulkan/context.rs
@@ -15,12 +15,14 @@ pub struct Context {
     _surface: Arc<Surface>,
     device: Arc<Device>,
     swapchain: Arc<Swapchain>,
+    allocator: Arc<gpu_allocator::vulkan::Allocator>,
 }
 
 #[derive(Debug)]
 pub enum ContextCreateError {
     InstanceError(InstanceCreateError),
     VkError(ash::vk::Result),
+    AllocatorError(gpu_allocator::AllocationError),
     NoDevice,
 }
 
@@ -36,6 +38,12 @@ impl From<ash::vk::Result> for ContextCreateError {
     }
 }
 
+impl From<gpu_allocator::AllocationError> for ContextCreateError {
+    fn from(value: gpu_allocator::AllocationError) -> Self {
+        ContextCreateError::AllocatorError(value)
+    }
+}
+
 impl Display for ContextCreateError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -46,6 +54,9 @@ impl Display for ContextCreateError {
                 write!(f, "vulkan error: {vk_error}")
             }
             ContextCreateError::NoDevice => write!(f, "no suitable device found"),
+            ContextCreateError::AllocatorError(allocation_error) => {
+                write!(f, "gpu allocation error: {allocation_error}")
+            }
         }
     }
 }
@@ -76,11 +87,24 @@ impl Context {
             window,
         )?);
 
+        let alloc_create_desc = gpu_allocator::vulkan::AllocatorCreateDesc {
+            instance: instance.handle().clone(),
+            device: device.handle().clone(),
+            physical_device: device.physical_device().handle(),
+            debug_settings: gpu_allocator::AllocatorDebugSettings::default(),
+            // TODO: yeah this should be supported.
+            buffer_device_address: true,
+            allocation_sizes: gpu_allocator::AllocationSizes::default(),
+        };
+
+        let allocator = Arc::new(gpu_allocator::vulkan::Allocator::new(&alloc_create_desc)?);
+
         Ok(Self {
             _instance: instance,
             _surface: surface,
             device,
             swapchain,
+            allocator,
         })
     }
 
